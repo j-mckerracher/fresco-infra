c9ec08872a9c65dadd3412670c0517be7ec0b5b8
Josh <jmckerra@purdue.edu>
2024-10-23 10:48:23 -0600

    update

--- variables.tf
+++ variables.tf
@@ -7,33 +7,30 @@ variable "aws_region" {
 variable "db_username" {
   description = "Username for the database"
   type        = string
-  default     = "kDj3jXr5zJjskYavXmGX7H"
+  sensitive   = true
 }
 
 variable "db_password" {
   description = "Password for the database"
   type        = string
   sensitive   = true
-  default     = "bH3F&NUA%TYexnaMxGDws$"
 }
 
 variable "db_name" {
   description = "Database name"
   type        = string
-  default     = "fresco"
 }
 
 variable "jwt_secret" {
   description = "Secret key used to sign JWT tokens"
   type        = string
   sensitive   = true
-  default     = "your-very-secure-jwt-secret"
 }
 
 variable "jwt_issuer" {
   description = "Issuer for JWT tokens"
   type        = string
-  default     = "my-local-test-app"
+  sensitive   = true
 }
 
 variable "use_existing_private_subnets" {
